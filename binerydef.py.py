74ee3d13139c40cb558364e13363c47e7ac7e52f
melissa <153478174+melissamosayebi@users.noreply.github.com>
2024-01-26 15:54:10 +0330

    Update binerydef.py
    new change

--- binerydef.py.py
+++ binerydef.py.py
@@ -1,12 +1,12 @@
-a=int(input(": "))
-count=0
-s=0
-while a//2>0:
-    b=a%2
-    s=(s*0.1)+b
-    count+=1
-    a//=2
-s=(s*0.1)+a
-s*=10**count
-s=int(s)
-print(s)
+def binery(a):
+    count=-1
+    s=0
+    while a>0:
+        b=a%2
+        s=(s*0.1)+b
+        count+=1
+        a//=2
+    s*=10**count
+    s=int(s)
+    print(s)
+binery(int(input(": ")))
