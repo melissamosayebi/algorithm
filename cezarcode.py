88e480d6388e3fe0377ed93724bf6403e4ad72fd
melissa <153478174+melissamosayebi@users.noreply.github.com>
2024-02-15 09:02:51 +0330

    Add files via upload

---
+++ cezarcode.py
@@ -0,0 +1,11 @@
+from string import ascii_letters as ac
+file=open("filegen.txt","r")
+content=file.read()
+st=""
+for i in content:
+    if i in ac:
+        s=ac.index(i)
+        st+=ac[(s+2)%52]
+    else:
+        st+=i
+print(st)
